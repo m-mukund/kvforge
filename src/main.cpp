d5c07e86f4ca1475bde9520c7bb5b8c1cb470f6d
M Mukund <mukundm8pp@gmail.com>
2026-06-09 18:52:20 -0600

    Fixing header files to run on Ubuntu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <sys/_types/_ssize_t.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
